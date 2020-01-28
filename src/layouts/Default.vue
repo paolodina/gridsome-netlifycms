6c4f1aefd0e081687d104027538ee77085da1f09
Paolo Dina <paolo.dina@codingmoth.com>
2020-01-28 18:32:35 +0100

    layout cleanup

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
     <slot/>
   </div>
 </template>
@@ -23,28 +14,20 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
+  background-color: #f4f4f4;
+  padding: 0;
+  margin: 0;
 }
 
-.layout {
-  max-width: 760px;
+.container {
+  width: 90%;
   margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
 }
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
+@media screen and (min-width: 992px) {
+  .container {
+    width: 60%;
+    padding: 30px 0px;
+  }
 }
 </style>
